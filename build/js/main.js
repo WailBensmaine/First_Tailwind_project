7b259fd5b3194f670e751dae227daa6e2812ea90
WailBensmaine <w_bensmaine@estin.dz>
2023-12-13 22:20:51 +0100

    This commit contains the work on the animation and fixes come errors in the previous code

---
+++ build/js/main.js
@@ -0,0 +1,12 @@
+const initApp = () => {
+    const humburgerBtn = document.getElementById('huburger-button');
+    const mobileMenu = document.getElementById('mobile-menu');
+    const toggleMenu = () => {
+        mobileMenu.classList.toggle('hidden');
+        mobileMenu.classList.toggle('flex');
+    }
+    humburgerBtn.addEventListener('click', toggleMenu)
+    mobileMenu.addEventListener('click', toggleMenu)
+}
+
+document.addEventListener('DOMContentLoaded', initApp)
